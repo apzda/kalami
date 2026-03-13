be3a44d47b1e3407ccebcfbb190295c59dd993ef
ninggf <windywany@gmail.com>
2026-03-13 11:19:43 +0800

    add snail-job server

---
+++ app/snailjob-server/README.md
@@ -0,0 +1,11 @@
+# SNAIL JOB SERVER
+
+Version: 1.9.0
+
+启动成功后，访问管理控制台：
+
+* 地址：[http://localhost:8989/snail-job](http://localhost:8989/snail-job)
+* 默认账号：admin
+* 默认密码：admin
+
+更多内容移步[官方文档](https://snailjob.opensnail.com/)
--- kalami/kalami-spring-boot-rocketmq-starter/src/test/java/com/apzda/kalami/mq/rocket/test/listener/DemoMessageListener.java
+++ kalami/kalami-spring-boot-rocketmq-starter/src/test/java/com/apzda/kalami/mq/rocket/test/listener/DemoMessageListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 the original author or authors.
+ * Copyright 2026 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,30 +15,14 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * Copyright (C) 2023-2025 Fengz Ning (windywany@gmail.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package com.apzda.kalami.mq.rocket.test.listener;
 
 import com.apzda.kalami.mq.rocket.listener.IMessageListener;
 import com.apzda.kalami.mq.rocket.test.Tags;
 import com.apzda.kalami.mq.rocket.test.message.SimpleMessage;
+import jakarta.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.apis.message.MessageView;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author fengz (windywany@gmail.com)
@@ -49,7 +33,7 @@ import org.jetbrains.annotations.NotNull;
 public class DemoMessageListener implements IMessageListener<SimpleMessage, Tags> {
 
     @Override
-    public boolean onMessage(@NotNull SimpleMessage message, Tags tag, @NotNull MessageView messageView) {
+    public boolean onMessage(@Nonnull SimpleMessage message, Tags tag, @Nonnull MessageView messageView) {
         log.info("Receive message from topic: {}, tag: {}, message: {}", messageView.getTopic(), tag,
                 message.getContent());
         return true;
